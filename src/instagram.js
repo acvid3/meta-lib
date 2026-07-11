9ee1f3254a4df04fa65f3c4395a742e14010ce74
acvid3 <acvid3@gmail.com>
2026-07-11 14:49:20 +0300

    fix: use graph.instagram.com for IGAA tokens

--- src/instagram.js
+++ src/instagram.js
@@ -1,4 +1,5 @@
-const GRAPH_API = 'https://graph.facebook.com/v19.0';
+const IG_API = 'https://graph.instagram.com/v21.0';
+const FB_API = 'https://graph.facebook.com/v19.0';
 
 async function createMediaContainer(accessToken, igUserId, options) {
   const { imageUrl, videoUrl, caption, mediaType, children } = options;
@@ -16,7 +17,7 @@ async function createMediaContainer(accessToken, igUserId, options) {
     params.image_url = imageUrl;
   }
 
-  const res = await fetch(`${GRAPH_API}/${igUserId}/media`, {
+  const res = await fetch(`${IG_API}/${igUserId}/media`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(params),
@@ -31,7 +32,7 @@ async function createMediaContainer(accessToken, igUserId, options) {
 }
 
 async function publishMedia(accessToken, igUserId, creationId) {
-  const res = await fetch(`${GRAPH_API}/${igUserId}/media_publish`, {
+  const res = await fetch(`${IG_API}/${igUserId}/media_publish`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ creation_id: creationId, access_token: accessToken }),
@@ -64,7 +65,7 @@ async function postCarousel(accessToken, igUserId, childrenIds, caption) {
 }
 
 async function getMediaStatus(accessToken, mediaId) {
-  const res = await fetch(`${GRAPH_API}/${mediaId}?fields=id,media_type,media_url,permalink,caption,timestamp&access_token=${accessToken}`);
+  const res = await fetch(`${IG_API}/${mediaId}?fields=id,media_type,media_url,permalink,caption,timestamp&access_token=${accessToken}`);
   const data = await res.json();
   if (!res.ok || data.error) {
     throw Object.assign(new Error(data.error?.message || 'Fetch failed'), { response: { status: res.status, data } });
